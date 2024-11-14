aed402ce3866e45ac8b39f1734d46d81b540b39c
l.c <108731242+loaychlih@users.noreply.github.com>
2024-11-14 16:35:30 +0100

    Update runrdd.sh

--- pagerankrdd/runrdd.sh
+++ pagerankrdd/runrdd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ##create bucket
 
-
+# Créer le cluster avec changement de workers pour chaque experience par nous
 gcloud dataproc clusters create cluster-spark \
     --enable-component-gateway \
     --region europe-west1 \
@@ -12,12 +12,7 @@ gcloud dataproc clusters create cluster-spark \
     --image-version 2.0-debian10 \
     --project project-test-440719
 
-
-
-## create the cluster 2 workers
-gcloud dataproc clusters create cluster-a35a --enable-component-gateway --region europe-west1 --zone europe-west1-c --master-machine-type n1-standard-4 --master-boot-disk-size 500 --num-workers 2 --worker-machine-type n1-standard-4 --worker-boot-disk-size 500 --image-version 2.0-debian10 --project master-2-large-scale-data
-## run
-## (suppose that out directory is empty !!)
+# Soumettre le job PySpark
 gcloud dataproc jobs submit pyspark --region europe-west1 --cluster cluster-spark gs://pagerankloay2/pagerankrdd.py -- gs://public_lddm_data/small_page_links.nt 3
 
 ## access results
